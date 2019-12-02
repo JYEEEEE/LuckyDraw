9c1b9042fa0501f4e78ceeaf7be9cc4896e7afcc
jyeeee <807806181@qq.com>
2019-12-02 20:39:56 +0800

    feat: 增加数据库默认主键_id字段的支持，并添加update方法

--- database/models.py
+++ database/models.py
@@ -5,6 +5,7 @@
 @contact: yan.zhao@idiaoyan.com
 @time: 2019/10/14 16:55
 """
+from bson import ObjectId
 from pymongo import MongoClient
 
 client = MongoClient('192.168.10.21', 27017)
@@ -19,13 +20,16 @@ class BaseModel(object):
     def to_mongo(self):
         mongo_dict = {}
         for k, v in self.__dict__.items():
-            if not k.startswith('_'):
+            if k == '_id':
+                if not v:
+                    # 如果没有给定_id,那么由数据库自动生成
+                    continue
                 mongo_dict[k] = v
         return mongo_dict
 
     @classmethod
     def get_fields(cls):
-        return filter(lambda x: not x.startswith('_'), cls.__dict__)
+        return filter(lambda x: not x.startswith('_') or x == '_id', cls.__dict__)
 
     @classmethod
     def get_collection(cls):
@@ -61,17 +65,25 @@ class BaseModel(object):
 
     def save(self):
         mongo_dict = self.to_mongo()
-        return self.get_collection().insert_one(mongo_dict)
+        if '_id' in mongo_dict:
+            _id = mongo_dict.pop('_id')
+            if not isinstance(_id, ObjectId):
+                _id = ObjectId(_id)
+            return self.get_collection().update_one({'_id': _id}, mongo_dict, upsert=True)
+        else:
+            return self.get_collection().insert_one(mongo_dict)
 
 
 class User(BaseModel):
     """
     用户表
     """
+    _id: ObjectId = None
     login_name: str = None
     login_pass: str = None
 
 
 class Rule(BaseModel):
+    _id: ObjectId = None
     name: str = ""
     comment: str = ""
